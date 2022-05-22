001a1690e0d8182cad7e5691050fd1b352774f4e
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-23 02:17:37 +0600

    react router and navbar added

---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2 className='text-2xl'>This is About us</h2>
+        </div>
+    );
+};
+
+export default About;
