92b6bcc1e11a7fc438ae11e1dc200afd0a7aec2d
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-23 01:24:10 +0600

    initialized project and added tailwind, daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#0D6EFD",
+
+                    secondary: "#F7CD2E",
+
+                    accent: "#6562d1",
+
+                    neutral: "#271F2D",
+
+                    "base-100": "#FFFFFF",
+
+                    info: "#53D8F3",
+
+                    success: "#2CDD8D",
+
+                    warning: "#F5A52E",
+
+                    error: "#FB283A",
+                },
+            },
+        ],
+    },
+    plugins: [require("daisyui")],
+};
