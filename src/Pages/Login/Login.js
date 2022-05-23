5eb9808e2ced729a2b494f140b33cdd271778b43
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-24 03:32:43 +0600

    google sign in added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import auth from "../../Firebase/firebase.init";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 
 const Login = () => {
+    const [signInWithGoogle, googleUser, googleLoading, googleError] =
+        useSignInWithGoogle(auth);
+    if (googleUser) {
+        console.log(googleUser);
+    }
     return (
         <div>
-            <button className='btn btn-primary px-8'>Sign in with google</button>
+            <button
+                onClick={() => signInWithGoogle()}
+                className="btn btn-primary px-8 text-white"
+            >
+                Sign in with google
+            </button>
         </div>
     );
 };
