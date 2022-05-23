434defdfab2493581582ccba36e80d47bcf337df
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-23 16:39:41 +0600

    car parts added on homepage

---
+++ src/Pages/Home/Part/Part.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Part = ({ part }) => {
+    const {
+        name,
+        img,
+        price,
+        description,
+        minOrderQuantity,
+        availableQuantity,
+    } = part;
+    return (
+        <div className="card lg:card-side bg-base-100 shadow-xl">
+            <figure>
+                <img
+                    className="md:h-full md:w-80 object-cover"
+                    src={img}
+                    alt="car-part"
+                />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>Price: ${price}</p>
+                <p>{description.length > 100 ? description.slice(0, 100) + '...' : description}</p>
+                <p>Minimum Order Quantity: {minOrderQuantity}</p>
+                <p>Available Quantity: {availableQuantity}</p>
+                <div className="card-actions justify-end mt-1">
+                    <button className="btn btn-primary">Place Order</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Part;
--- src/Pages/Home/Parts/Parts.js
+++ src/Pages/Home/Parts/Parts.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaTools } from "react-icons/fa";
+import Part from "../Part/Part";
 
 const Parts = () => {
+    const [parts, setParts] = useState([]);
+    useEffect(() => {
+        fetch("parts.json")
+            .then(res => res.json())
+            .then(data => setParts(data))
+    }, [])
     return (
         <div className="mt-10">
-            <h2 className="text-center text-slate-700 text-5xl">
+            <h2 className="text-center text-slate-500 text-5xl">
                 <span>
                     Parts{" "}
                     <FaTools className="inline-block text-2xl mb-1"></FaTools>
                 </span>
             </h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-12">
+                {
+                    parts.map(part => <Part part={part} key={part._id}></Part>)
+                }
+            </div>
         </div>
     );
 };
