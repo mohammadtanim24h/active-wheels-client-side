2f15868252c3390101b1e131334c4e28d1781466
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-24 03:20:20 +0600

    firebase and login page added

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <button className='btn btn-primary px-8'>Sign in with google</button>
+        </div>
+    );
+};
+
+export default Login;
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -16,6 +16,9 @@ const Navbar = () => {
             <li>
                 <NavLink to="/contact">Contact</NavLink>
             </li>
+            <li>
+                <NavLink to="/login">Login</NavLink>
+            </li>
         </>
     );
     return (
