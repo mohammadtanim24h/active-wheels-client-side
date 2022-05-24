6fe55927c1464082ccfd458d392255c26c290b7d
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-25 03:35:12 +0600

    part info and order info added in purchase page

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import auth from "../../Firebase/firebase.init";
 import Loading from "../Shared/Loading";
 
 const Purchase = () => {
     const { id } = useParams();
+    const [user] = useAuthState(auth);
     const { data: part, isLoading } = useQuery(["part", id], () =>
         fetch(`http://localhost:5000/part/${id}`).then((res) => res.json())
     );
@@ -12,20 +15,79 @@ const Purchase = () => {
         return <Loading></Loading>;
     }
     return (
-        <div>
-            <div className="card w-80 md:w-96 bg-base-100 shadow-xl">
-                <figure>
-                    <img src={part.img} alt="Shoes" />
-                </figure>
+        <div className="flex flex-col md:flex-row justify-around mt-8">
+            <div className="card w-80 md:w-1/3 h-max bg-base-100 shadow-xl mx-auto md:mx-0">
+                <img src={part.img} alt="" />
                 <div className="card-body">
                     <h2 className="card-title">{part.name}</h2>
                     <p>Price: ${part.price}</p>
                     <p>{part.description}</p>
-                    <p>Minimum Quantity: ${part.minOrderQuantity}</p>
-                    <p>Available Quantity: ${part.availableQuantity}</p>
-                    <div className="card-actions justify-end">
-                        <button className="btn btn-primary">Buy Now</button>
-                    </div>
+                    <p>Minimum Quantity: {part.minOrderQuantity}</p>
+                    <p>Available Quantity: {part.availableQuantity}</p>
+                </div>
+            </div>
+            <div className="card w-80 md:w-1/3 bg-base-100 shadow-xl mx-auto md:mx-0">
+                <div className="card-body">
+                    <h2 className="text-2xl text-center text-slate-600 mb-2">
+                        Order Information
+                    </h2>
+                    <form>
+                        <div className="form-control w-full max-w-lg mb-1">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                value={user.displayName}
+                                disabled
+                                className="input input-bordered w-full max-w-lg mb-1"
+                            />
+                        </div>
+                        <div className="form-control w-full max-w-lg mb-1">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                value={user.email}
+                                disabled
+                                className="input input-bordered w-full max-w-lg mb-1"
+                            />
+                        </div>
+                        <div className="form-control w-full max-w-lg mb-1">
+                            <label className="label">
+                                <span className="label-text">Address</span>
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Address"
+                                className="input input-bordered w-full max-w-lg mb-1"
+                            />
+                        </div>
+                        <div className="form-control w-full max-w-lg mb-1">
+                            <label className="label">
+                                <span className="label-text">Phone</span>
+                            </label>
+                            <input
+                                type="number"
+                                placeholder="Phone"
+                                className="input input-bordered w-full max-w-lg mb-1"
+                            />
+                        </div>
+                        <div className="form-control w-full max-w-lg mb-1">
+                            <label className="label">
+                                <span className="label-text">Quantity</span>
+                            </label>
+                            <input
+                                type="number"
+                                placeholder="Quantity"
+                                defaultValue={part.minOrderQuantity}
+                                className="input input-bordered w-full max-w-lg mb-1"
+                            />
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
