9c1071f9849237e6b45e5842c030b19b30b40570
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-25 18:12:43 +0600

    api added to send orderinfo to server

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -19,6 +19,33 @@ const Purchase = () => {
     const quantityChange = (e) => {
         setQuantity(e.target.value);
     };
+
+    const handlePurchase = (e) => {
+        e.preventDefault();
+        const orderInfo = {
+            partName: part.name,
+            partId: part._id,
+            price: part.price,
+            quantity: e.target.quantity.value,
+            email: user.email,
+            clientName: user.displayName,
+            address: e.target.address.value,
+            phone: e.target.phone.value,
+        };
+
+        // add orderInfo to db
+        fetch("http://localhost:5000/part", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(orderInfo),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+            });
+    };
     return (
         <div className="flex flex-col md:flex-row justify-around mt-8">
             <div className="card w-80 md:w-1/3 h-max bg-base-100 shadow-xl mx-auto md:mx-0">
@@ -36,7 +63,7 @@ const Purchase = () => {
                     <h2 className="text-2xl text-center text-slate-600 mb-2">
                         Order Information
                     </h2>
-                    <form>
+                    <form onSubmit={handlePurchase}>
                         <div className="form-control w-full max-w-lg mb-1">
                             <label className="label">
                                 <span className="label-text">Name</span>
@@ -67,8 +94,10 @@ const Purchase = () => {
                             </label>
                             <input
                                 type="text"
+                                name="address"
                                 placeholder="Address"
                                 className="input input-bordered w-full max-w-lg mb-1"
+                                required
                             />
                         </div>
                         <div className="form-control w-full max-w-lg mb-1">
@@ -77,8 +106,10 @@ const Purchase = () => {
                             </label>
                             <input
                                 type="number"
+                                name="phone"
                                 placeholder="Phone"
                                 className="input input-bordered w-full max-w-lg mb-1"
+                                required
                             />
                         </div>
                         <div className="form-control w-full max-w-lg mb-1">
@@ -88,6 +119,7 @@ const Purchase = () => {
                             <input
                                 type="number"
                                 placeholder="Quantity"
+                                name="quantity"
                                 defaultValue={part.minOrderQuantity}
                                 onChange={quantityChange}
                                 className="input input-bordered w-full max-w-lg mb-1"
@@ -104,6 +136,7 @@ const Purchase = () => {
                             </p>
                         )}
                         <button
+                            type="submit"
                             disabled={
                                 quantity < part.minOrderQuantity ||
                                 quantity > part.availableQuantity
