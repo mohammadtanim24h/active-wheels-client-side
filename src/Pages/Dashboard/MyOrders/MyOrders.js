67a5a6998be0397458edd6b105036802e72aafd3
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-26 20:39:52 +0600

    token verification added in my orders page

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,11 +1,15 @@
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import toast from "react-hot-toast";
 import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
 import auth from "../../../Firebase/firebase.init";
 import Loading from "../../Shared/Loading";
 import OrderRow from "./OrderRow/OrderRow";
 
 const MyOrders = () => {
+    const navigate = useNavigate();
     const [user] = useAuthState(auth);
     const email = user?.email;
     const {
@@ -13,7 +17,21 @@ const MyOrders = () => {
         isLoading,
         refetch,
     } = useQuery("orders", () =>
-        fetch(`http://localhost:5000/order/${email}`).then((res) => res.json())
+        fetch(`http://localhost:5000/order/${email}`, {
+            method: "GET",
+            headers: {
+                authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+        }).then((res) => {
+            if (res.status === 401 || res.status === 403) {
+                toast.error("Forbidden access, please login again!");
+                signOut(auth);
+                localStorage.removeItem("accessToken");
+                navigate("/");
+            } else {
+                return res.json();
+            }
+        })
     );
 
     if (isLoading) {
