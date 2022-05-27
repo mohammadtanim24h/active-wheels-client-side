a80448a7d304223444f17c1597ac0f52f69f052b
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-27 18:31:23 +0600

    portfolio page updated

--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./MyPortfolio.css";
 
 const MyPortfolio = () => {
     return (
@@ -6,17 +7,17 @@ const MyPortfolio = () => {
             <h2 className="text-center text-blue-500 font-semibold text-3xl">
                 My Portfolio
             </h2>
-            <div className="w-3/4 mx-auto mt-5 shadow-lg rounded p-8 leading-6">
-                <h3 className="text-slate-600">
+            <div className="w-full md:w-3/4 mx-auto mt-5 shadow-lg rounded p-8 leading-6 my-info md:text-slate-100">
+                <h3>
                     <span className="font-semibold">Name:</span> Mohammad Tanim
                 </h3>
-                <h3 className="text-slate-600">
+                <h3>
                     <span className="font-semibold">Email:</span>{" "}
                     mohammad.tanim24h@gmail.com
                 </h3>
-                <h3 className="text-slate-600">
-                    <span className="font-semibold">Education:</span> Passed
-                    HSC. Now admission candidate.
+                <h3>
+                    <span className="font-semibold">Education:</span> Passed HSC
+                    (Business Studies). Now admission candidate.
                 </h3>
                 <h3>
                     <span className="font-semibold">Skills:</span>
@@ -33,41 +34,43 @@ const MyPortfolio = () => {
                     <span className="font-semibold">Tools:</span> Git, Github,
                     Firebase, Netlify, Heroku, Codepen, Npm
                 </h4>
-                <div class="divider"></div>
-                <h3 className="font-semibold">Three of my Projects: </h3>
-                <h4>
-                    Perfumania:{" "}
-                    <a
-                        href="https://perfumania-4c8ba.web.app/"
-                        rel="noreferrer"
-                        target="_blank"
-                        className="text-primary underline"
-                    >
-                        Live site
-                    </a>
-                </h4>
-                <h4>
-                    Gym Freak:{" "}
-                    <a
-                        href="https://gym-freak-a8e55.web.app/"
-                        rel="noreferrer"
-                        target="_blank"
-                        className="text-primary underline"
-                    >
-                        Live site
-                    </a>
-                </h4>
-                <h4>
-                    Snap Away:{" "}
-                    <a
-                        href="https://snap-away-862c61.netlify.app/"
-                        rel="noreferrer"
-                        target="_blank"
-                        className="text-primary underline"
-                    >
-                        Live site
-                    </a>
-                </h4>
+
+                <div className="mt-6">
+                    <h3 className="font-semibold">Three of my Projects: </h3>
+                    <h4>
+                        Perfumania:{" "}
+                        <a
+                            href="https://perfumania-4c8ba.web.app/"
+                            rel="noreferrer"
+                            target="_blank"
+                            className="md:text-emerald-200 font-semibold"
+                        >
+                            Live site
+                        </a>
+                    </h4>
+                    <h4>
+                        Gym Freak:{" "}
+                        <a
+                            href="https://gym-freak-a8e55.web.app/"
+                            rel="noreferrer"
+                            target="_blank"
+                            className="md:text-emerald-200 font-semibold"
+                        >
+                            Live site
+                        </a>
+                    </h4>
+                    <h4>
+                        Snap Away:{" "}
+                        <a
+                            href="https://snap-away-862c61.netlify.app/"
+                            rel="noreferrer"
+                            target="_blank"
+                            className="md:text-emerald-200 font-semibold"
+                        >
+                            Live site
+                        </a>
+                    </h4>
+                </div>
             </div>
         </div>
     );
