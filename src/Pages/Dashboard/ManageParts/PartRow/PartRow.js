37ec7ee2f3d7306fe03fae039d3e96fa84c9766b
Mohammad Tanim <mohammad.tanim24h@gmail.com>
2022-05-27 20:27:46 +0600

    part delete api added

--- src/Pages/Dashboard/ManageParts/PartRow/PartRow.js
+++ src/Pages/Dashboard/ManageParts/PartRow/PartRow.js
@@ -1,7 +1,42 @@
 import React from "react";
+import Swal from "sweetalert2";
 
-const PartRow = ({part, index, refetch}) => {
-    const {name, price, minOrderQuantity, availableQuantity} = part;
+const PartRow = ({ part, index, refetch }) => {
+    const { _id, name, price, minOrderQuantity, availableQuantity } = part;
+    const deletePart = () => {
+        Swal.fire({
+            title: "Are you sure you want to delete?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#d33",
+            cancelButtonColor: "#3DBE29",
+            confirmButtonText: "Yes, delete it!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/part/${_id}`, {
+                    method: "DELETE",
+                    headers: {
+                        authorization: `Bearer ${localStorage.getItem(
+                            "accessToken"
+                        )}`,
+                    },
+                })
+                    .then((res) => res.json())
+                    .then((data) => {
+                        if (data.deletedCount === 1) {
+                            Swal.fire(
+                                "Deleted!",
+                                "Part has been deleted.",
+                                "success"
+                            );
+                            refetch();
+                        }
+                    });
+            }
+        });
+    };
+
     return (
         <tr>
             <th>{index + 1}</th>
@@ -9,7 +44,11 @@ const PartRow = ({part, index, refetch}) => {
             <td>${price}/p</td>
             <td>{minOrderQuantity}</td>
             <td>{availableQuantity}</td>
-            <td><button className="btn btn-sm btn-error">Delete</button></td>
+            <td>
+                <button onClick={deletePart} className="btn btn-sm btn-error">
+                    Delete
+                </button>
+            </td>
         </tr>
     );
 };
